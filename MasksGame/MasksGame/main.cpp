47b85517426decbf661c672a0bfe22b7a1880a39
KojinKuro <kamidefire.keni@gmail.com>
2015-03-16 14:16:10 -0700

    Initial commit to master
    Created start of the game

---
+++ MasksGame/MasksGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MasksGame
+//
+//  Created by Charles Kwang on 3/16/15.
+//  Copyright (c) 2015 Charles Kwang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
